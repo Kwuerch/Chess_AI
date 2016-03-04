546e70098128efb40d756112d1cb855cea74e960
heatsink <seanswally@gmail.com>
2016-03-03 19:17:30 -0800

    Add Cell & Human

---
+++ Cell.java
@@ -0,0 +1,13 @@
+/**
+ *  ** Cell Class **
+ *
+ * Represents the cell location (col, row)
+ *
+ * @author Kyle Wuerch, Sean Wallace
+ * @version Program 7
+ */
+public class Cell{
+   char col;
+   int row;
+
+}
