3cb00c4bc7214b42dc864c61b69616e51d61076e
heatsink <seanswally@gmail.com>
2016-03-11 22:19:18 -0800

    Add functions to KnightIterator

--- src/board/KnightIterator.java
+++ src/board/KnightIterator.java
@@ -27,21 +27,63 @@ public class KnightIterator extends BoardIterator<ChessPiece>{
 	 * @param int the desired starting index of the iterator
 	 */
 	public KnightIterator(ChessPiece[] board, int direction, int index){
+		switch(direction){
+			case UP_LEFT:
+				buffer = 19;
+				break;
+			case UP_RIGHT:
+				buffer = 21;
+				break;
+			case DOWN_LEFT:
+				buffer = -21;
+				break;
+			case DOWN_RIGHT:
+				buffer = -19;
+				break;
+		}
 		this.index = index;
 		this.board = board;
 	}
 	
+	/**
+ 	 * Determines whether there is another spot in the direction specified
+	 * @return boolean true if there is a next element
+ 	 */
 	public boolean hasNext(){
+		ChessPiece p = board[index + buffer];
+
+		if (p == null){
+			return true;
+		}else if(!p.toString().equals("I")){
+			return true;
+		}
 		return false;
 	}
+
+	/**
+	 * Gets the next chesspiece in the iterator and adds to the iterator index
+	 * @return the next ChessPiece
+ 	 */
 	public ChessPiece next(){
-		return board[0];
+		if (!hasNext()){
+			throw new NoSuchElementException();
+		}
+		index += buffer;
+		return board[index];
 	}
 	
+	/**
+	 * Gets the current index
+	 * @return the current integer index of the iterator
+ 	 */
 	public int index(){
 		return index;
 	}
 
+	/**
+	 * This (remove) method is not supported and throws an exception
+	 * @throws UnsupportedOperationException
+ 	 */
 	public void remove(){
 		throw new UnsupportedOperationException();
 	}
